7fb07dd1fc309b5035647e53ca8ca0104a9f9e92
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2024-03-26 21:47:14 -0300

    Add mock places

--- src/components/Lugares.jsx
+++ src/components/Lugares.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react';
 
 import SearchBar from './SearchBar';
+import Button from './Button';
+import { IconContext } from 'react-icons';
+import { IoIosSearch } from 'react-icons/io';
+import PlaceCard from './PlaceCard';
+
+const amountResults = 14;
 
 export default function Lugares() {
   const [query, setQuery] = useState('');
@@ -11,17 +17,42 @@ export default function Lugares() {
     setQuery(text);
   }
 
+  const mockPlaces = [];
+
+  for (let i = 0; i < amountResults; i++) {
+    const data = {
+      name: 'Lorem ipsum',
+      imgSrc: 'https://source.unsplash.com/random/?valparaiso' + '&' + i,
+      id: i,
+    };
+    mockPlaces.push(data);
+  }
+
   return (
-    <div className="min-h-screen">
-      <div className="mx-auto max-w-5xl px-8 py-10">
-        <h1 className="font-playfair text-3xl font-semibold text-slate-900">
+    <div className="min-h-screen bg-stone-100">
+      <div className="mx-auto max-w-5xl px-8 py-8">
+        <h1 className="font-playfair text-4xl font-semibold text-slate-900">
           Lugares
         </h1>
-        <form onSubmit={search}>
+        <form onSubmit={search} className="mt-8 flex">
           <SearchBar setText={setText} />
-          {/*<Activities />*/}
-          <button type="submit">Hi</button>
+          <Button
+            type="submit"
+            variant="ghost"
+            className="flex aspect-square items-center justify-center rounded-none rounded-br-xl rounded-tr-xl bg-blue-900 px-4 hover:bg-sky-600"
+          >
+            <IconContext.Provider value={{ size: '1.5em', color: '#fff' }}>
+              <IoIosSearch />
+            </IconContext.Provider>
+          </Button>
         </form>
+        <div>
+          <div className="mt-8 grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-8">
+            {mockPlaces.map((place) => {
+              return <PlaceCard key={place.id} data={place} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
